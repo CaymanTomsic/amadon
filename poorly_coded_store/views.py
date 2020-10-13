ab2719e29e2705b1bf5cc3493e9a3b16fa1e2f0e
CaymanTomsic <caymantomsic@gmail.com>
2020-10-12 19:32:52 -0700

    Made changes, completed assignment

--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 
 def index(request):
@@ -9,8 +9,22 @@ def index(request):
 
 def checkout(request):
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
+    price_from_form = float(Product.objects.get(id= int(request.POST["price"])).price)
     total_charge = quantity_from_form * price_from_form
     print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    created_order = Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+    print(created_order)
+    return redirect(f"/success/{created_order.id}")
+
+def success(request, order_id):
+    total_spent= 0
+    total_quantity = 0
+    for order in Order.objects.all():
+        total_spent += order.total_price
+        total_quantity += order.quantity_ordered
+    context = {
+        'order_price' : Order.objects.get(id=order_id).total_price,
+        'total_spent': total_spent,
+        'total_quantity': total_quantity
+    }
+    return render(request, "store/checkout.html", context)
